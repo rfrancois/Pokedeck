d52363714069b5728052a52b806252857d680038
rfrancois <f.romain2009@gmail.com>
2015-12-01 15:08:27 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub test
 		Trainer c = new Trainer();
 		Energy e = new Energy();
 		System.out.println(e);
